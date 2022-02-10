0338f50cf0a35df86f07952b0010c9d9703b7235
bishoy melek <bishoymelekwadie@gmail.com>
2022-02-10 23:42:17 +0200

    hot fix

--- context/apollo-client.js
+++ context/apollo-client.js
@@ -1,9 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: `${
-    process.env.NEXT_PUBLIC_SERVER_API || 'http://localhost:1337/graphql'
-  }`,
+  uri:
+    `${process.env.NEXT_PUBLIC_SERVER_API + '/graphql'}` ||
+    'http://localhost:1337/graphql',
   cache: new InMemoryCache(),
 });
 
